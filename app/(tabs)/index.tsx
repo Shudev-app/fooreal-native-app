2364c17c6a5044cb455434f497375f671dceff19
Shu01 <aquashuworld@gmail.com>
2026-06-05 14:25:58 +0900

    test commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,6 +7,7 @@ import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { Link } from 'expo-router';
 
+//test
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
