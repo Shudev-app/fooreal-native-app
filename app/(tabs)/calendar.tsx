82234d554f98e77e09a4f9410eac7603a7865472
Shu01 <aquashuworld@gmail.com>
2026-06-06 18:19:24 +0900

    各タブのヘッダーの実装

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,7 +1,40 @@
+import Header from "@/components/common/Header";
+import { Ionicons } from "@expo/vector-icons";
+import { Pressable, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function TabTwoScreen() {
+export default function CalendarScreen() {
   return (
-    <SafeAreaView></SafeAreaView>
+    <SafeAreaView style={{
+      flex: 1,
+      backgroundColor: "#FFF3E6"
+      }}>
+      <Header 
+        actions={
+          <View style={{
+            flexDirection: "row",
+            gap: 16,
+          }}>
+            <Pressable>
+              <Ionicons 
+                name="calendar-outline"
+                size={28}
+                color="#333"
+                />
+            </Pressable>
+
+            <Pressable>
+              <Ionicons 
+                name="ellipsis-horizontal"
+                size={28}
+                color="#333"
+              />
+            </Pressable>
+          </View>
+          
+        }
+      />
+
+    </SafeAreaView>
   );
 }
--- app/(tabs)/list.tsx
+++ app/(tabs)/list.tsx
@@ -1,3 +1,42 @@
+import Header from "@/components/common/Header";
+import { Ionicons } from "@expo/vector-icons";
+import { Pressable, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
 export default function ListScreen() {
-    return null;
+    return(
+        <SafeAreaView style={{
+              flex: 1,
+              backgroundColor: "#FFF3E6"
+              }}>
+              <Header 
+                actions={
+                  <View style={{
+                    flexDirection: "row",
+                    gap: 16,
+                  }}>
+                    <Pressable>
+                      <Ionicons 
+                        name="search-outline"
+                        size={28}
+                        color="#333"
+                        />
+                    </Pressable>
+        
+                    <Pressable>
+                      <Ionicons 
+                        name="ellipsis-horizontal"
+                        size={28}
+                        color="#333"
+                      />
+                    </Pressable>
+                  </View>
+                  
+                }
+              />
+        
+            </SafeAreaView>
+
+    );
 }
